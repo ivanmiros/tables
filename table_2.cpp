a03f96438565a32a8ba151c45b65be9fec8b1e21
ivanmiros1 <ivan.malynyak@sleepnumber.com>
2024-11-17 18:38:36 +0100

    add basic functions

--- table_2.cpp
+++ table_2.cpp
@@ -3,51 +3,77 @@
 #include <cstdio>    // printf, getchar
 #include <cstring>   // C-style string by char type 
 
+// 2 3 1 2 3 4 9 8 7
 
 
-int main() {
-     
-    int **A;
-    int start_rows;
+void init_arrays(int** &A, int* &B, int &rows) {
 
+    // Get number of rows
+    scanf("%d", &rows);
+    A = new int* [rows];  // Array with Data
+    B = new int [rows];   // Array with Column counts 
 
-    // 1. Init arrays
-    scanf("%d", &start_rows);
-    A = new int* [start_rows];
-
-    int B[start_rows];
-
-    for (int i=0; i<start_rows; i++) {
+    // Get column size and fill it
+    for (int i=0; i<rows; i++) {
         scanf("%d", &B[i]);
         A[i] = new int[B[i]];
         for (int j=0; j<B[i]; j++) {
            scanf("%d", &A[i][j]); 
         }
     }
+}
 
-    for (int i=0; i<start_rows; i++) {
+void print_arrays(int** &A, int* &B, int &rows) {
+    printf("\nRows: %d\n", rows);
+    for (int i=0; i<rows; i++) {
+        printf("%d: ",B[i]);
         for (int j=0; j<B[i]; j++) {
             printf("%d ",A[i][j]);
         }
         printf("\n");
     }
+}
+
+void remove_row(int** &A, int* &B, int &rows, int row_remove) {
+    if (row_remove > rows) return;
+    rows --;
+
+    int **newA = new int* [rows];
+    for (int i=0; i<row_remove-1; i++) newA[i] = A[i];
+    for (int i=row_remove-1; i<rows; i++) newA[i] = A[i+1];    
+    delete [] A;
+    A = newA;
+
+    int *newB = new int [rows];
+    for (int i=0; i<row_remove-1; i++) newB[i] = B[i];
+    for (int i=row_remove-1; i<rows; i++) newB[i] = B[i+1];    
+    delete [] B;
+    B = newB;
+}
+
+
+
+int main() {
+
+    int **A;
+    int *B;
+    int rows;
+
+
+    // 1. Init arrays
+    init_arrays(A, B, rows);
+    print_arrays(A, B, rows);
           
     // 2. Main code      
-    
-
-
-
-
-
-
-
-
+    remove_row(A, B, rows, 1);
+    print_arrays(A, B, rows);
 
 
     // 9. Delete arrays
-    for (int i=0; i<start_rows; i++) {
+    for (int i=0; i<rows; i++) {
         delete [] A[i];
     }
     delete [] A;
+    delete [] B;
 
 }
